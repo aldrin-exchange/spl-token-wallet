2c7d07e0b3f9190ae335eca42c994cbe11852bf7
aldrin <support@aldrin.com>
2021-11-25 14:57:53 +0200

    UP

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,6 @@ import { Container, Wrapper } from './components/CommonStyledComponents'
 const Welcome = lazy(() => import('./routes/Welcome'))
 
 // const LOCAL_BUILD = window.location.href.includes('localhost');
-
-const a = 1
-
-a.toNum()
 export const Pages = () => {
   useMemo(() => {
     const params = new URLSearchParams(window.location.hash.slice(1))
